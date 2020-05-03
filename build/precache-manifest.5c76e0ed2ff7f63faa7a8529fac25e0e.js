24df638a205d2577bb16fb2d632f0dbad1cccdf8
Yuriy Vakulik <yuriy.vakulik@just-eat.com>
2020-05-03 21:27:32 +0300

    initial

---
+++ build/precache-manifest.5c76e0ed2ff7f63faa7a8529fac25e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e02e89235fdcd179271fe6f11d7323b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff72d9ab5f388e2d99c",
+    "url": "/static/css/main.5922b3f2.chunk.css"
+  },
+  {
+    "revision": "b5b734056f93cf4f02d3",
+    "url": "/static/js/2.b1327c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1327c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff72d9ab5f388e2d99c",
+    "url": "/static/js/main.6c755f35.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
